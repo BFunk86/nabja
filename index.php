11b1924fd254505a641dc755ece95a899926ba34
Brandon Young <youngbd01@sunyjcc.edu>
2017-05-11 19:49:43 -0400

    Fixed BS Styles

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <nav class="navbar navbar-default" role="navigation">
         <div class="container-fluid">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -40,15 +40,15 @@
                 </button>
                 <a class="navbar-brand visible-xs" href="#">JCC</a>
             </div>
-            <div class="collapse navbar-collapse navbar-collapse">
+            <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Home</a></li>
                     <li><a href="report.html">Report</a></li>
-                    <li><a  class="active" href="create.html">Create</a></li>
+                    <li><a href="create.html">Create</a></li>
                     <li><a href="relations.html">Relations</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li id="login"><a href="#login">Login <i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
+                    <li id="login"><a href="#login" data-toggle="modal" data-target="#loginModal">Login <i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div>
@@ -63,50 +63,97 @@
         <form action="report.php" method="post">
             <fieldset class="fieldSet">
                 <legend id="courseLegend">
-                    <h2>Course</h2></legend>
-                <label for="course">Course Name:</label><br>
-                <select id="course" size="6" multiple>
-                    <option value="web_pub">Web Publishing</option>
-                    <option value="micro_Apps_I">Microcomputer Applications I</option>
-                    <option value="prog_concepts">Programming Concepts/Applications</option>
-                    <option value="comp_prog">Computer Programming</option>
-                    <option value="web_tech">Web Technologies</option>
-                    <option value="micro_apps_II">Microcomputer Applications II</option>
-                </select>
-                <br><br>
-                <input class="submitButton" type="button" name="courseSubmit" value="Generate Report" />
+                    <h2>Course</h2>
+                </legend>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <div class="form-group">
+                            <label for="course">Course Name:</label><br>
+                            <select id="course" class="form-control" size="6" multiple>
+                                <option value="web_pub">Web Publishing</option>
+                                <option value="micro_Apps_I">Microcomputer Applications I</option>
+                                <option value="prog_concepts">Programming Concepts/Applications</option>
+                                <option value="comp_prog">Computer Programming</option>
+                                <option value="web_tech">Web Technologies</option>
+                                <option value="micro_apps_II">Microcomputer Applications II</option>
+                            </select>
+                        </div><!-- .form-group -->
+                    </div><!-- col-xs-4 -->
+                </div><!-- .row -->
+                <input class="btn" type="button" name="courseSubmit" value="Generate Report" />
             </fieldset>
         </form>
-        <form>
+        <form action="report.php" method="post">
             <fieldset class="fieldSet">
                 <legend id="programLegend">
                     <h2>Program</h2>
                 </legend>
-                <label for="program">Program Name:</label>
-                <select name="program" id="program">
-                    <option value="cs">Computer Science</option>
-                    <option value="it">Information Technology</option>
-                    <option value="cis">Computer Information Systems</option>
-                </select>
-                <br><br>
-                <input class="submitButton" type="button" name="programSubmit" value="Generate Report" />
+                <div class="row">
+                    <div class="col-xs-4">
+                        <div class="form-group">
+                            <label for="program">Program Name:</label>
+                            <select name="program" id="program" class="form-control">
+                                <option value="cs">Computer Science</option>
+                                <option value="it">Information Technology</option>
+                                <option value="cis">Computer Information Systems</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <input class="btn" type="button" name="programSubmit" value="Generate Report" />
             </fieldset>
         </form>
-        <form>
+        <form action="report.php" method="post">
             <fieldset class="fieldSet">
                 <legend id="genEdLegend">
-                    <h2>General Education</h2></legend>
-                <label for="genEd">General Education Name:</label>
-                <select name="genEd" id="genEd">
-                    <option value="math">Mathematics/Quantitive Reasoning</option>
-                    <option value="am_history">American History</option>
-                    <option value="arts">The Arts</option>
-                </select>
-                <br><br>
-                <input class="submitButton" type="button" name="generalSubmit" value="Generate Report" />
+                    <h2>General Education</h2>
+                </legend>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <div class="form-group">
+                            <label for="genEd">General Education Name:</label>
+                            <select name="genEd" id="genEd" class="form-control">
+                                <option value="math">Mathematics/Quantitive Reasoning</option>
+                                <option value="am_history">American History</option>
+                                <option value="arts">The Arts</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <input class="btn" type="button" name="generalSubmit" value="Generate Report" />
             </fieldset>
         </form>
     </fieldset>
-</form></div>
+</form><!-- outer form -->
+</div><!-- .container -->
+<div class="container" id="footer">
+    <p>SUNY Jamestown Community College - 525 Falconer Street, Jamestown, NY</p>
+</div>
+<div class="modal fade" tabindex="-1" role="dialog" id="loginModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Faculty Login</h4>
+            </div>
+            <div class="modal-body">
+                <form action="login" method="post">
+                    <div class="form-group">
+                        <label for="uName">User Name:</label>
+                        <input type="text" class="form-control" id="uName" name="uName">
+                    </div>
+                    <div class="form-group">
+                        <label for="passwd">Password:</label>
+                        <input type="password" class="form-control" id="passwd" name="passwd">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn">Login</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 </body>
 </html>
